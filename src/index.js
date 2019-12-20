0d6aabb7ec28a07b19240cbcbc0abc4580ea65dc
AyoAlfonso <alfonsoayo7@gmail.com>
2019-12-20 13:37:04 +0100

    Upgrade node engine

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 module.exports = function ({
-  env,
-  status,
-  www,
+	env,
+	status,
+	www,
 }) {
-  const environment = env || 'production';
-  const statusCode = status || 302;
+	const environment = env || 'production';
+	const statusCode = status || 302;
 
-  return function (req, res, next) {
-    if (process.env.NODE_ENV === environment) {
-      const host = req.header('host');
-      if (www) {
-        const correctHost = host.match(/^www\..*/i);
-        if (!correctHost) {
-          return res.redirect(301, `https://www.${host}`);
-        }
-      }
+	return function (req, res, next) {
+		if (process.env.NODE_ENV === environment) {
+			const host = req.header('host');
+			if (www) {
+				const correctHost = host.match(/^www\..*/i);
+				if (!correctHost) {
+					return res.redirect(301, `https://www.${host}`);
+				}
+			}
 
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(statusCode, `https://${req.hostname}${req.originalUrl}`);
-      }
-      next();
-    } else {
-      next();
-    }
-  };
+			if (req.headers['x-forwarded-proto'] !== 'https') {
+				return res.redirect(statusCode, `https://${req.hostname}${req.originalUrl}`);
+			}
+			next();
+		} else {
+			next();
+		}
+	};
 };
